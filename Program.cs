0afea87e0b8db3201fe3773c00fca3e1487d925f
Artem <130403999+Art-ad-hub@users.noreply.github.com>
2023-04-11 22:18:43 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -15,18 +15,15 @@ namespace Working_with_strings
             int userAge;
             string userZodiac;
             string userWork;
-            string userAgeRead;
-            
+
             Console.Write("Введите свое имя: ");
             userName = Console.ReadLine();
             Console.Write("Введите свой возраст: ");
-            userAgeRead = Console.ReadLine();
-            userAge = Convert.ToInt32(userAgeRead);
+            userAge = Convert.ToInt32(Console.ReadLine());
             Console.Write("Введите свой знак зодиака: ");
             userZodiac = Console.ReadLine();
             Console.Write("Введите ваше место работы: ");
             userWork = Console.ReadLine();
-
             Console.WriteLine($"Ваше имя {userName}, Вам {userAge} лет и по знаку задиака вы {userZodiac},а место вашей работы {userWork}.");
             Console.ReadKey();
         }
